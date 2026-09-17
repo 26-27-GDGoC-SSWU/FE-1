86964cf0b17345f68ba0f7c6610fecae3abd5cfc
SeYoon0118 <seyoonamy0118@gmail.com>
2026-09-17 19:35:44 +0900

    feat:week2_배열 생성하여 map()적용

--- SeYoon0118/src/App.jsx
+++ SeYoon0118/src/App.jsx
@@ -41,12 +41,16 @@ export default function App() {
       <section>
         <h2>학교 가는 길</h2>
 
-        <Station name="충무로" />
-        <Station name="동대문역사문화공원" />
-        <Station name="동대문" />
-        <Station name="혜화" />
-        <Station name="한성대입구" />
-        <Station name="성신여대입구" />
+        {stations.map(
+          (
+            station, //stations 배열을 하나씩 꺼내서 꺼낸역 하나를 'station'이라 부르고
+          ) => (
+            <Station
+              key={station.id}
+              name={station.name} //그 데이터로 Station 컴포넌트를 하나 만든다
+            />
+          ),
+        )}
       </section>
     </main>
   );
