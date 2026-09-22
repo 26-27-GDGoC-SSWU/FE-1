021827a14f734d6e3552a815bf04c7aca5941d3d
alicena2ppn2 <na2ppn26450@gmail.com>
2026-09-22 13:38:39 +0900

    feat: week1 setup react project

---
+++ yunkyung/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>1주차 실습입니다.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ yunkyung/src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>ReactStudy</h1>;
+}
+
+export default Header;
